fd8d3d018d222ebd21170920f3c6b5bcc5726d58
Cezary Bros <cezary.bros@gmail.com>
2020-09-03 18:14:39 +0200

    Initial project setup

---
+++ ChannelEngineApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ChannelEngineApp
+{
+    static class Program
+    {
+        private static IConfiguration _config;
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            CreateConfiguration();
+            ConfigureClient();
+
+            await StartApp();
+        }
+        private static void CreateConfiguration()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+        }
+
+        private static void ConfigureClient()
+        {
+            _httpClient.BaseAddress = new Uri($"{_config["AppConfig:ApiBaseUri"]}{_config["AppConfig:ApiVersion"]}");
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", _config["AppConfig:UserAgent"]);
+        }
+
+        private static async Task StartApp()
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
