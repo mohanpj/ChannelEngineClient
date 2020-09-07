5b3104bf49bd0bb9f019b48eb4772f3d881dffc8
Cezary Bros <cezary.bros@gmail.com>
2020-09-07 21:17:34 +0200

    Add session storage, Add top products sold

--- ChannelEngineWebApp/Startup.cs
+++ ChannelEngineWebApp/Startup.cs
@@ -25,6 +25,7 @@ namespace ChannelEngineWebApp
             services.AddServerSideBlazor();
             services.AddMediatR(typeof(Startup).Assembly);
             services.AddSharedServices(Configuration);
+            services.AddProtectedBrowserStorage();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
